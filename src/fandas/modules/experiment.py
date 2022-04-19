82bed1ad61b45bd29c70138b0eae3863b5d036a2
rodrigo <rvhonorato@gmail.com>
2022-04-19 15:26:32 +0200

    refactor `Experiment` to use preset experiments

--- src/fandas/modules/experiment.py
+++ src/fandas/modules/experiment.py
@@ -1,10 +1,10 @@
+import copy
 import itertools
 import logging
-
+import os
 import sys
-import copy
 
-from fandas.modules.chemical import ATOM_LIST, ATOM_REF, QUANTUM_RELATIONSHIP
+from fandas.modules.chemical import ATOM_LIST, ATOM_REF, EXPERIMENT_CATALOG
 
 log = logging.getLogger("fandaslog")
 
@@ -14,10 +14,11 @@ class Experiment:
 
     def __init__(self, chemical_shift, input_data_dic):
         self.chemical_shift = chemical_shift
-        self.experiment_dic = input_data_dic["experiments"]
+        self.experiment_list = input_data_dic["preset_experiments"]["selected"]
         filtered_shift = None
         distance_file = input_data_dic["distance"]["distance_fname"]
         cutoff = input_data_dic["distance"]["distance_cutoff"]
+
         if input_data_dic["distance"]["distance_fname"]:
             filtered_shift = self.filter_by_distance(
                 distance_fname=distance_file,
@@ -27,7 +28,7 @@ class Experiment:
         self.filtered_chemical_shift = filtered_shift
 
     def filter_by_distance(self, distance_fname, cutoff):
-        """."""
+        """Filter the chemical shifts by distance."""
 
         distance_dic = self.read_distance_file(distance_fname, cutoff)
 
@@ -39,30 +40,31 @@ class Experiment:
                 for atom in self.chemical_shift.residues[resnum].shifts:
                     if atom not in distance_dic[resnum]:
                         del filtered.residues[resnum].shifts[atom]
+
         return filtered
 
     def run_experiments(self):
         """Run all the experiments."""
-        for i, exp in enumerate(self.experiment_dic, start=1):
+        for i, nmr_exp_notation in enumerate(self.experiment_list, start=1):
 
-            atoms = self.experiment_dic[exp]["atoms"]
-            if "inter" in self.experiment_dic[exp]:
-                inter = self.experiment_dic[exp]["inter"]
+            atoms, direction = self.retrieve_exp_info(nmr_exp_notation)
+
+            log.info(f"Running experiment {i} {nmr_exp_notation}")
+            if "DQ" in nmr_exp_notation or "SQ" in nmr_exp_notation:
+                quantum = True
             else:
-                inter = False
+                quantum = False
 
-            exp_id = "_".join(atoms)
-
-            log.info(f"Running experiment {i} {exp_id}")
-
-            self.execute(atoms, exp_id=exp_id, inter=inter)
+            self.execute(atoms, direction, exp_id=nmr_exp_notation, quantum=quantum)
 
             if self.filtered_chemical_shift:
-                exp_id += "_dist"
+                exp_id = f"{nmr_exp_notation}_dist"
                 log.info(f"Running experiment {i} {exp_id}")
-                self.execute(atoms, exp_id=exp_id, inter=inter, filtered=True)
+                self.execute(
+                    atoms, direction, exp_id=exp_id, filtered=True, quantum=quantum
+                )
 
-    def execute(self, atoms, exp_id, inter=False, filtered=False):
+    def execute(self, atoms, direction, exp_id, quantum=False, filtered=False):
         """Execute the experiment based on an atom list."""
         results = []
         if filtered:
@@ -70,21 +72,18 @@ class Experiment:
         else:
             shifts = self.chemical_shift
 
-        dimension = len(atoms)
-        if "DQ" in atoms:
-            if dimension == 2:
-                atom_iter = QUANTUM_RELATIONSHIP
+        if quantum:
+            atom_iterable = atoms
         else:
             atom_list = self.translate_atoms(atoms)
-            atom_iter = list(itertools.product(*atom_list))
+            atom_iterable = list(itertools.product(*atom_list))
 
-        shift_iterable = self.make_iter(shifts, atom_iter, dimension, inter=inter)
+        shift_iterable = self._make_iter(shifts, atom_iterable, direction)
 
         results = []
         for comb in shift_iterable:
             result_line = self._make_line(comb)
             if result_line:
-                print(result_line)
                 results.append(result_line)
 
         if not results:
@@ -93,53 +92,46 @@ class Experiment:
             self._write_output(results, exp_id)
 
     def _make_line(self, data_t):
-        """."""
+        """Generate the line to be written to the output file."""
         notes = ""
         values = []
         for i, atom_name in zip(data_t[0], data_t[1]):
             notes += f"{i.resname}{i.resnum}{atom_name}-"
             value = self.get_value(i.shifts, atom_name)
-            values.append(value)
+            if value:
+                values.append(value)
 
-        if all(values):
+        if all(values) and len(values) == len(data_t[0]):
             value_str = "\t".join(map("{:.2f}".format, values))
-            line = f"{notes[:-1]}\t{value_str}"
+            line = f"{notes[:-1]}\t{value_str}" + os.linesep
+
             return line
 
-    def make_iter(self, shifts, atom_list, dimension, inter=False):
-        """."""
+    def _make_iter(self, shifts, atom_list, direction):
+        """Make an iterable to be used by the experiment executor."""
         resnum_list = list(shifts.residues.items())
         first_resum = resnum_list[0][0]
         last_resnum = resnum_list[-1][0]
 
-        # dimension = len(atom_list[0])
+        dimension = len(direction)
 
         combinations = []
-        if dimension == 2:
-            for r in range(first_resum, last_resnum + 1):
-                r1 = shifts.residues[r]
-                if inter:
-                    for i in [0, 1, -1]:
-                        try:
-                            r2 = shifts.residues[r - i]
-                            combinations.append((r1, r2))
-                        except KeyError:
-                            pass
-                else:
-                    combinations.append((r1, r1))
+        for resnum in range(first_resum, last_resnum + 1):
+            for e in itertools.product(*direction):
+                try:
+                    if dimension == 3:
+                        residue_1 = shifts.residues[resnum - e[0]]
+                        residue_2 = shifts.residues[resnum - e[1]]
+                        residue_3 = shifts.residues[resnum - e[2]]
+                        combinations.append((residue_1, residue_2, residue_3))
+                    elif dimension == 2:
+                        residue_1 = shifts.residues[resnum - e[0]]
+                        residue_2 = shifts.residues[resnum - e[1]]
+                        combinations.append((residue_1, residue_2))
 
-        elif dimension == 3:
-            for r in range(first_resum, last_resnum - 1):
-                r1 = shifts.residues[r]
-                if inter:
-                    for i in [0, 1, -1]:
-                        try:
-                            r2 = shifts.residues[r - i]
-                            combinations.append((r1, r1, r2))
-                        except KeyError:
-                            pass
-                else:
-                    combinations.append((r1, r1, r1))
+                except KeyError:
+                    # there was no shift found in a given index, skip it
+                    pass
 
         return list(itertools.product(combinations, atom_list))
 
@@ -162,12 +154,30 @@ class Experiment:
         return translated_atom_l
 
     def get_value(self, shift_dic, atom):
-        """."""
-        return sum([shift_dic[v] for v in atom.split("+")])
+        """Get the shift values."""
+        try:
+            value = sum([shift_dic[v] for v in atom.split("+")])
+        except KeyError:
+            value = 0.0
+
+        return value
+
+    @staticmethod
+    def retrieve_exp_info(nmr_notation, catalog=EXPERIMENT_CATALOG):
+        """Retrieve the atoms and direction from the experiment catalog."""
+        for experiment in catalog:
+            if catalog[experiment]["nmr_notation"] == nmr_notation:
+                direction = catalog[experiment]["direction"]
+                if "DQ" in nmr_notation or "SQ" in nmr_notation:
+                    atoms = catalog[experiment]["atom_relation"]
+                else:
+                    atoms = catalog[experiment]["atoms"]
+
+        return atoms, direction
 
     @staticmethod
     def _write_output(results, exp_id):
-        """."""
+        """Write the output file."""
         output_fname = f"{exp_id}_exp_.txt"
         log.info(f"Saving output to {output_fname}")
         with open(output_fname, "w") as fh:
@@ -176,7 +186,7 @@ class Experiment:
 
     @staticmethod
     def read_distance_file(distance_fname, cutoff):
-        """."""
+        """Read the distance file."""
         dist_dic = {}
         with open(distance_fname, "r") as fh:
             # 1,CA,1,C,1.548
@@ -184,7 +194,6 @@ class Experiment:
                 data = line.split(",")
                 resnum_a, atom_a, resnum_b, atom_b, distance = data
                 resnum_a = int(resnum_a)
-                # resnum_b = int(resnum_b)
                 distance = float(distance)
                 if distance <= cutoff:
                     if resnum_a not in dist_dic:
